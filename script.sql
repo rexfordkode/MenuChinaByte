ddc92ad552a6bc1a86cd1f6d63b20e35a510d798
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2021-11-23 04:06:33 +0000

    Initial commit

---
+++ script.sql
@@ -0,0 +1,271 @@
+--  1 Creating a table and Primary keys
+
+CREATE TABLE restaurant (
+  id integer PRIMARY KEY,
+  name varchar(20),
+ description varchar(100),
+ rating decimal,
+ telephone char(20),
+ hours varchar(100)
+);
+
+CREATE TABLE address(
+  id integer PRIMARY KEY,
+  street_number varchar(10),
+  street_name varchar(20),
+  city varchar(20),
+  state varchar(15),
+  google_map_link varchar(50),
+  restaurant integer REFERENCES address(id)
+);
+
+-- Task 2
+CREATE TABLE category(
+  id char(2),
+  name varchar(20),
+  desription varchar(200),
+  CONSTRAINT category_id PRIMARY KEY (id, name)
+);
+
+CREATE TABLE dish (
+   id integer,
+   name varchar(50),
+   description varchar(200),
+   hot_and_spicy boolean,
+   CONSTRAINT dish_id PRIMARY KEY (id, name)
+);
+
+CREATE TABLE review (
+   id integer PRIMARY KEY,
+   rating decimal,
+   description varchar(100),
+   date date,
+   restaurant_id integer REFERENCES restaurant(id)
+ );
+ CREATE TABLE categories_dishes(
+   name char(5),
+   id integer ,
+   price money
+ );
+
+--  Task 3
+/* 
+ *--------------------------------------------
+ Insert values for restaurant
+ *--------------------------------------------
+ */
+INSERT INTO restaurant VALUES (
+  1,
+  'Bytes of China',
+  'Delectable Chinese Cuisine',
+  3.9,
+  '6175551212',
+  'Mon - Fri 9:00 am to 9:00 pm, Weekends 10:00 am to 11:00 pm'
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for address
+ *--------------------------------------------
+ */
+INSERT INTO address VALUES (
+  1,
+  '2020',
+  'Busy Street',
+  'Chinatown',
+  'MA',
+  'http://bit.ly/BytesOfChina',
+  1
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for review
+ *--------------------------------------------
+ */
+INSERT INTO review VALUES (
+  1,
+  5.0,
+  'Would love to host another birthday party at Bytes of China!',
+  '05-22-2020',
+  1
+);
+
+INSERT INTO review VALUES (
+  2,
+  4.5,
+  'Other than a small mix-up, I would give it a 5.0!',
+  '04-01-2020',
+  1
+);
+
+INSERT INTO review VALUES (
+  3,
+  3.9,
+  'A reasonable place to eat for lunch, if you are in a rush!',
+  '03-15-2020',
+  1
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for category
+ *--------------------------------------------
+ */
+INSERT INTO category VALUES (
+  'C',
+  'Chicken',
+  null
+);
+
+INSERT INTO category VALUES (
+  'LS',
+  'Luncheon Specials',
+  'Served with Hot and Sour Soup or Egg Drop Soup and Fried or Steamed Rice  between 11:00 am and 3:00 pm from Monday to Friday.'
+);
+
+INSERT INTO category VALUES (
+  'HS',
+  'House Specials',
+  null
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for dish
+ *--------------------------------------------
+ */
+INSERT INTO dish VALUES (
+  1,
+  'Chicken with Broccoli',
+  'Diced chicken stir-fried with succulent broccoli florets',
+  false
+);
+
+INSERT INTO dish VALUES (
+  2,
+  'Sweet and Sour Chicken',
+  'Marinated chicken with tangy sweet and sour sauce together with pineapples and green peppers',
+  false
+);
+
+INSERT INTO dish VALUES (
+  3,
+  'Chicken Wings',
+  'Finger-licking mouth-watering entree to spice up any lunch or dinner',
+  true
+);
+
+INSERT INTO dish VALUES (
+  4,
+  'Beef with Garlic Sauce',
+  'Sliced beef steak marinated in garlic sauce for that tangy flavor',
+  true
+);
+
+INSERT INTO dish VALUES (
+  5,
+  'Fresh Mushroom with Snow Peapods and Baby Corns',
+  'Colorful entree perfect for vegetarians and mushroom lovers',
+  false
+);
+
+INSERT INTO dish VALUES (
+  6,
+  'Sesame Chicken',
+  'Crispy chunks of chicken flavored with savory sesame sauce',
+  false
+);
+
+INSERT INTO dish VALUES (
+  7,
+  'Special Minced Chicken',
+  'Marinated chicken breast sauteed with colorful vegetables topped with pine nuts and shredded lettuce.',
+  false
+);
+
+INSERT INTO dish VALUES (
+  8,
+  'Hunan Special Half & Half',
+  'Shredded beef in Peking sauce and shredded chicken in garlic sauce',
+  true
+);
+
+/*
+ *--------------------------------------------
+ Insert valus for cross-reference table, categories_dishes
+ *--------------------------------------------
+ */
+INSERT INTO categories_dishes VALUES (
+  'C',
+  1,
+  6.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'C',
+  3,
+  6.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'LS',
+  1,
+  8.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'LS',
+  4,
+  8.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'LS',
+  5,
+  8.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'HS',
+  6,
+  15.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'HS',
+  7,
+  16.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'HS',
+  8,
+  17.95
+);
+
+-- Queries Bellow
+
+SELECT 
+restaurant.name,
+ restaurant.telephone,
+  address.street_name 
+  FROM restaurant
+   JOIN address 
+   ON restaurant.id = address.id;
+
+SELECT 
+MAX(rating) 
+AS best_rating 
+FROM restaurant;
+
+-- 
+
+
+
+FROM
+   information_schema.key_column_usage
+ WHERE
+   table_name = 'category';
+
+
